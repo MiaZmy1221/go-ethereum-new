0ad90bf6fd429efba492eea70568c5782f2a5306
Mengya <zhang.9407@osu.edu>
2021-02-26 18:41:23 +0800

    te

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -837,6 +837,9 @@ func makeLog(size int) executionFunc {
 		}
 
 		d := callContext.memory.GetCopy(int64(mStart.Uint64()), int64(mSize.Uint64()))
+		fmt.Printf("mStart %d \n", int64(mStart.Uint64())
+		fmt.Printf("mSize %d \n", int64(mSize.Uint64())
+		fmt.Printf("memory %s \n", callContext.memory.Data())
 
 		interpreter.evm.StateDB.AddLog(&types.Log{
 			Address: callContext.contract.Address(),
