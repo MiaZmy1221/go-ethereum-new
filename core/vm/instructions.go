46d6375f081122e8b221f04f02cbbdb4246ea51a
Mengya <zhang.9407@osu.edu>
2021-02-26 15:50:30 +0800

    test1

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -22,7 +22,7 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/holiman/uint256"
 	"golang.org/x/crypto/sha3"
-	"fmt"
+	// "fmt"
 )
 
 func opAdd(pc *uint64, interpreter *EVMInterpreter, callContext *callCtx) ([]byte, error) {
