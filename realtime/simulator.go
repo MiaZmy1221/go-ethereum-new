f5e44be99d7ce88d03f083e8411c5b03f2dadbaf
Mengya <zhang.9407@osu.edu>
2021-03-31 20:33:19 +0800

    combine

--- realtime/simulator.go
+++ realtime/simulator.go
@@ -1,16 +1,16 @@
 package realtime
 
 import (
-	"fmt"
-	"math/big"
-	"time"
+	// "fmt"
+	// "math/big"
+	// "time"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	// "github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/consensus"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/types"
+	// "github.com/ethereum/go-ethereum/core"
+	// "github.com/ethereum/go-ethereum/core/state"
+	// "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/eth/downloader"
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/ethereum/go-ethereum/log"
