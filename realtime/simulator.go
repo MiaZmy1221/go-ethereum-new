f436bfdd5dae3b8fe671c1638faf1341c357c7ea
Mengya <zhang.9407@osu.edu>
2021-04-07 18:05:30 +0800

    combine

--- realtime/simulator.go
+++ realtime/simulator.go
@@ -8,6 +8,8 @@ import (
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/common"
+	"os"
 )
 
 // Backend wraps all methods required for mining.
