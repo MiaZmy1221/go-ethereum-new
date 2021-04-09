86d0c04ba26970d7502bf26f29095b15ad401a43
Mengya <zhang.9407@osu.edu>
2021-04-09 13:27:27 +0800

    combine

--- core/state_processor.go
+++ core/state_processor.go
@@ -459,7 +459,7 @@ func rtapplyTransaction(msg types.Message, config *params.ChainConfig, bc ChainC
 	}
 	
 	// ignore this step so far
-	var root []bytevar root []byte
+	var root []byte
 	if config.IsByzantium(header.Number) {
 		fmt.Println("IsByzantium")
 	} else {
