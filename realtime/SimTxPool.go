7a72a0e223133f76eb315dd7dfb67c3afdab59ff
Mengya <zhang.9407@osu.edu>
2021-04-12 00:20:13 +0800

    combine

--- realtime/SimTxPool.go
+++ realtime/SimTxPool.go
@@ -6,6 +6,8 @@ import (
 
 	// "github.com/ethereum/go-ethereum/consensus"
 	// "github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/consensus"
+	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/common"
